2e31926d75aafe49d48044059e69d42e63e70721
Abderrezak-WEBDEV <razik.bouiche@gmail.com>
2023-05-25 22:26:32 +0200

    commit 2

--- server/index.js
+++ server/index.js
@@ -7,11 +7,12 @@ require("dotenv").config({path: "./.env"});
 //connexion db
 require("./config/database");
 
+//json parsing
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-//app.use("/", require("./routes/post.route"));
+app.use("/", require("./routes/post.route"));
 
 app.listen(process.env.PORT, ()=>{
     console.log(`serveur est sur le port ${process.env.PORT}`)
